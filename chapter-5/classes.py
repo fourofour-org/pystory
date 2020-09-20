0b76ce96c6dc273e08579814e9cfcd53a9401cb6
rio <gorishabhio@gmail.com>
2020-09-20 22:37:20 +0530

    added classes

---
+++ chapter-5/classes.py
@@ -0,0 +1,28 @@
+# basic class syntax in Pyhton 
+
+
+
+class Human: 
+
+    def __init__(self):
+        pass 
+
+    def speak(self):
+        pass 
+
+    def walk(self):
+        pass 
+
+    def eat(self):
+        pass 
+
+    def sleep(self):
+        pass 
+
+class Man(Human):
+
+    def __init__(self):
+        pass 
+
+    def speak(self):
+        print("Hey there! I'm a human")
